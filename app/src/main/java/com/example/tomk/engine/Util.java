9a7747ba521423fbc34f619954fe81719692c63f
PetoStr <petostrycek@centrum.sk>
2018-06-17 08:46:35 +0200

    added isVisible method

--- app/src/main/java/com/example/tomk/engine/Util.java
+++ app/src/main/java/com/example/tomk/engine/Util.java
@@ -1,6 +1,7 @@
 package com.example.tomk.engine;
 
 import com.example.tomk.engine.objects.Circle;
+import com.example.tomk.engine.objects.GameObject;
 import com.example.tomk.engine.objects.Pipe;
 
 /**
@@ -9,22 +10,34 @@ import com.example.tomk.engine.objects.Pipe;
 
 public class Util {
 
-    public boolean intersection(Circle circle, Pipe pipe) {
+    public static boolean intersection(Circle circle, Pipe pipe) {
 
-        circle.getPosition().x = Math.abs(circle.getPosition().x - pipe.getPosition().x);
-        circle.getPosition().y = Math.abs(circle.getPosition().y - pipe.getPosition().y);
+        float cx = Math.abs(circle.getPosition().x - pipe.getPosition().x);
+        float cy = Math.abs(circle.getPosition().y - pipe.getPosition().y);
 
-        if (circle.getPosition().x > (pipe.getSize().x/2 + circle.getSize().x)) { return false; }
-        if (circle.getPosition().y > (pipe.getSize().y/2 + circle.getSize().x)) { return false; }
+        if (cx > (pipe.getSize().x/2 + circle.getSize().x)) { return false; }
+        if (cy > (pipe.getSize().y/2 + circle.getSize().x)) { return false; }
 
-        if (circle.getPosition().x <= (pipe.getSize().x/2)) { return true; }
-        if (circle.getPosition().y <= (pipe.getSize().y/2)) { return true; }
+        if (cx <= (pipe.getSize().x/2)) { return true; }
+        if (cy <= (pipe.getSize().y/2)) { return true; }
 
-        float cornerDistance_sq = (float) ((Math.pow(circle.getPosition().x - pipe.getSize().x/2, 2.0)) +
-                        Math.pow((circle.getPosition().y - pipe.getSize().y/2), 2.0));
+        float cornerDistance_sq = (float) ((Math.pow(cx - pipe.getSize().x/2, 2.0)) +
+                        Math.pow((cy - pipe.getSize().y/2), 2.0));
 
         return (cornerDistance_sq <= (Math.pow(circle.getSize().x, 2.0)));
 
+    }
+
+    public static boolean isVisible(GameObject gameObject) {
+        float x = gameObject.getPosition().x;
+        float y = gameObject.getPosition().y;
+        float w = gameObject.getSize().x / 2;
+        float h = gameObject.getSize().y / 2;
+
+        return !(x + w > Screen.getWidth()
+                || x - w < -Screen.getWidth()
+                || y + h > Screen.getHeight()
+                || y - h < -Screen.getHeight());
     }
 
 }
